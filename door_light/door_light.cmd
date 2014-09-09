81d1375812c4d7ee5a528aa8b206c279368ceebb
Alex Gutikov <gutikoff@gmail.com>
2014-09-10 01:25:30 +0300

    door light for volvo

---
+++ door_light/door_light.cmd
@@ -0,0 +1,30 @@
+# Pin name action command file
+
+# Start of element CONN1
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element Q1
+ChangePinName(Q1, 2, b)
+ChangePinName(Q1, 1, c)
+ChangePinName(Q1, 3, e)
+
+# Start of element D2
+ChangePinName(D2, 2, 2)
+ChangePinName(D2, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element F1
+ChangePinName(F1, 1, 1)
+ChangePinName(F1, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
