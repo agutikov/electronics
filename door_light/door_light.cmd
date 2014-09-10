61c092e6702e910221b7a7eccc80e2f5bf847982
Alex Gutikov <gutikoff@gmail.com>
2014-09-10 13:13:32 +0300

    door light: add fuse holder and replace 3x2 pin to 4x2 pin

--- door_light/door_light.cmd
+++ door_light/door_light.cmd
@@ -1,9 +1,11 @@
 # Pin name action command file
 
 # Start of element CONN1
+ChangePinName(CONN1, 7, 7)
 ChangePinName(CONN1, 5, 5)
 ChangePinName(CONN1, 3, 3)
 ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 8, 8)
 ChangePinName(CONN1, 6, 6)
 ChangePinName(CONN1, 4, 4)
 ChangePinName(CONN1, 2, 2)
