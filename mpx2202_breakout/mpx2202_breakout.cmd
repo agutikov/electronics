556e85b2f9d3e19b3f97ef3a63144f2caced5c8c
Alex Gutikov <gutikoff@gmail.com>
2014-09-12 16:58:03 +0300

    MPX2202DP and breakout board for it

---
+++ mpx2202_breakout/mpx2202_breakout.cmd
@@ -0,0 +1,21 @@
+# Pin name action command file
+
+# Start of element D1
+ChangePinName(D1, 1, GND)
+ChangePinName(D1, 4, -Vout)
+ChangePinName(D1, 3, Vs)
+ChangePinName(D1, 2, +Vout)
+
+# Start of element CONN1
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+ChangePinName(CONN1, 3, 3)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
